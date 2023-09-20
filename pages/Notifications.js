68685d6f9525179a0862233f159ca080b4aa2107
rajashilan <rajashilan07@gmail.com>
2023-09-20 17:19:39 +0800

    feat(Notifications): added notifications page
    - with dummy data

---
+++ pages/Notifications.js
@@ -0,0 +1,465 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Pressable,
+  FlatList,
+  ScrollView,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  fontPixel,
+  widthPixel,
+  heightPixel,
+  pixelSizeVertical,
+  pixelSizeHorizontal,
+} from "../utils/responsive-font";
+import { Image } from "expo-image";
+import { StatusBar } from "expo-status-bar";
+
+import hamburgerIcon from "../assets/hamburger_icon.png";
+import SideMenu from "../components/SideMenu";
+import Modal from "react-native-modal";
+
+import IosHeight from "../components/IosHeight";
+
+import Toast from "react-native-toast-message";
+import { toastConfig } from "../utils/toast-config";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import Header from "../components/Header";
+
+const { width } = Dimensions.get("window");
+
+import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
+
+export default function Notifications({ navigation }) {
+  const dispatch = useDispatch();
+  const currentMember = useSelector(
+    (state) => state.data.clubData.currentMember
+  );
+  const club = useSelector((state) => state.data.clubData.club);
+  const loading = useSelector((state) => state.data.loading);
+  const campusID = useSelector((state) => state.data.campus.campusID);
+
+  const [isSideMenuVisible, setIsSideMenuVisible] = useState(false);
+
+  const [headerHeight, setHeaderHeight] = useState(300);
+  const [scrollHeight, setScrollHeight] = useState(0);
+  const [showMiniHeader, setShowMiniHeader] = useState(false);
+
+  const data = [
+    {
+      text: "has been approved! Add more details so others can see your club now.",
+      read: false,
+      sourceID: "E1FnMBdQlSQgC6rfcjy5",
+      sourceName: "Computer Science Club",
+      sourceDestination: "ClubsPages",
+      defaultText: "", //for normal notifications with no available source names
+    },
+    {
+      text: "",
+      read: true,
+      sourceID: "",
+      sourceName: "",
+      sourceDestination: "",
+      defaultText: "Congragulations on reaching 100 members!", //for normal notifications with no available source names
+    },
+  ];
+
+  const onLayout = (event) => {
+    const { x, y, height, width } = event.nativeEvent.layout;
+    setHeaderHeight(height);
+  };
+
+  useEffect(() => {
+    //if scroll height is more than header height and the header is not shown, show
+    if (scrollHeight > headerHeight && !showMiniHeader) setShowMiniHeader(true);
+    else if (scrollHeight < headerHeight && showMiniHeader)
+      setShowMiniHeader(false);
+  }, [scrollHeight, showMiniHeader]);
+
+  const toggleSideMenu = () => {
+    setIsSideMenuVisible(!isSideMenuVisible);
+  };
+
+  return (
+    <View style={styles.container}>
+      <IosHeight />
+      <View style={styles.headerContainerShowMiniHeader}>
+        {showMiniHeader ? (
+          <Animated.View
+            entering={FadeIn.duration(300)}
+            exiting={FadeOut.duration(300)}
+          >
+            <Text style={styles.headerMini} numberOfLines={1}>
+              notifications
+            </Text>
+          </Animated.View>
+        ) : (
+          <Text style={styles.headerMiniInvisible}>title</Text>
+        )}
+        <Pressable
+          onPress={toggleSideMenu}
+          hitSlop={{ top: 20, bottom: 40, left: 20, right: 20 }}
+        >
+          <Image
+            style={styles.hamburgerIcon}
+            source={hamburgerIcon}
+            contentFit="contain"
+          />
+        </Pressable>
+      </View>
+      <ScrollView
+        scrollEventThrottle={16}
+        stickyHeaderIndices={[1]}
+        onScroll={(event) => setScrollHeight(event.nativeEvent.contentOffset.y)}
+      >
+        <View style={{ width: "100%", flexDirection: "column" }}>
+          <View style={styles.paddingContainer}>
+            <View onLayout={onLayout}>
+              <Header header={"notifications"} />
+            </View>
+          </View>
+          <FlatList
+            style={{ marginBottom: pixelSizeVertical(32) }}
+            keyExtractor={(item, index) => index.toString()}
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            scrollEnabled={false}
+            data={data}
+            renderItem={({ item, index }) => (
+              <Pressable
+                onPress={() => {
+                  if (item.sourceDestination) {
+                    if (item.sourceDestination === "ClubsPages") {
+                      navigation.navigate("ClubsPages", {
+                        clubID: item.sourceID,
+                      });
+                    }
+                  }
+                }}
+              >
+                <View
+                  style={
+                    item.read
+                      ? styles.notificationContainerRead
+                      : styles.notificationContainerUnread
+                  }
+                >
+                  {item.defaultText ? (
+                    <Text
+                      style={{
+                        fontSize: fontPixel(14),
+                        fontWeight: "400",
+                        color: "#DFE5F8",
+                      }}
+                    >
+                      {item.defaultText}
+                    </Text>
+                  ) : (
+                    <Text style={{ lineHeight: 20 }}>
+                      <Text
+                        style={{
+                          fontSize: fontPixel(14),
+                          fontWeight: "600",
+                          color: "#07BEB8",
+                        }}
+                      >
+                        {item.sourceName}
+                      </Text>
+                      <Text
+                        style={{
+                          fontSize: fontPixel(14),
+                          fontWeight: "400",
+                          color: "#DFE5F8",
+                        }}
+                      >
+                        {" "}
+                        {item.text}
+                      </Text>
+                    </Text>
+                  )}
+                </View>
+              </Pressable>
+            )}
+          />
+        </View>
+      </ScrollView>
+
+      <Modal
+        isVisible={isSideMenuVisible}
+        onBackdropPress={toggleSideMenu} // Android back press
+        onSwipeComplete={toggleSideMenu} // Swipe to discard
+        animationIn="slideInRight" // Has others, we want slide in from the left
+        animationOut="slideOutRight" // When discarding the drawer
+        swipeDirection="left" // Discard the drawer with swipe to left
+        useNativeDriver // Faster animation
+        hideModalContentWhileAnimating // Better performance, try with/without
+        propagateSwipe // Allows swipe events to propagate to children components (eg a ScrollView inside a modal)
+        style={styles.sideMenuStyle} // Needs to contain the width, 75% of screen width in our case
+      >
+        <SideMenu
+          callParentScreenFunction={toggleSideMenu}
+          currentPage={"notifications"}
+          navigation={navigation}
+        />
+      </Modal>
+      <Toast config={toastConfig} />
+      <StatusBar style="light" translucent={false} backgroundColor="#0C111F" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0C111F",
+  },
+  paddingContainer: {
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+  },
+  imageHeaderContainer: {
+    height: pixelSizeVertical(120),
+    width: "100%",
+  },
+  overlayContainer: {
+    justifyContent: "center",
+    height: pixelSizeVertical(120),
+    width: "100%",
+    backgroundColor: "rgba(12, 17, 31, 0.7)",
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingTop: pixelSizeVertical(16),
+    paddingBottom: pixelSizeVertical(16),
+  },
+  header: {
+    fontSize: fontPixel(34),
+    fontWeight: "500",
+    color: "#DFE5F8",
+  },
+  loginButton: {
+    backgroundColor: "#07BEB8",
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingTop: pixelSizeVertical(18),
+    paddingBottom: pixelSizeVertical(18),
+    marginTop: pixelSizeVertical(16),
+    marginBottom: pixelSizeVertical(30),
+    width: "100%",
+    borderRadius: 5,
+  },
+  loginButtonText: {
+    fontSize: fontPixel(22),
+    fontWeight: "500",
+    color: "#0C111F",
+    textAlign: "center",
+  },
+  sideMenuStyle: {
+    margin: 0,
+    width: width * 0.85, // SideMenu width
+    alignSelf: "flex-end",
+  },
+  headerContainer: {
+    marginTop: pixelSizeVertical(20),
+    marginBottom: pixelSizeVertical(16),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+    alignItems: "center",
+  },
+  hamburgerIcon: {
+    height: pixelSizeVertical(20),
+    width: pixelSizeHorizontal(30),
+  },
+  backButton: {
+    fontSize: fontPixel(22),
+    fontWeight: "500",
+    color: "#C4FFF9",
+    marginTop: pixelSizeVertical(2),
+  },
+  headerMini: {
+    fontSize: fontPixel(22),
+    fontWeight: "500",
+    color: "#DFE5F8",
+    maxWidth: width - 180,
+  },
+  headerMiniInvisible: {
+    fontSize: fontPixel(22),
+    fontWeight: "500",
+    color: "#DFE5F8",
+    marginRight: pixelSizeHorizontal(16),
+    maxWidth: "80%",
+    opacity: 0,
+  },
+  headerContainerShowMiniHeader: {
+    marginTop: pixelSizeVertical(20),
+    marginBottom: pixelSizeVertical(8),
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: heightPixel(280),
+    marginBottom: pixelSizeVertical(12),
+    borderRadius: 5,
+  },
+  role: {
+    fontSize: fontPixel(14),
+    fontWeight: "400",
+    color: "#DFE5F8",
+    marginBottom: pixelSizeVertical(4),
+  },
+  name: {
+    fontSize: fontPixel(20),
+    fontWeight: "400",
+    color: "#DFE5F8",
+    marginBottom: pixelSizeVertical(10),
+  },
+  quote: {
+    fontSize: fontPixel(14),
+    fontWeight: "400",
+    color: "#C6CDE2",
+    lineHeight: 22,
+  },
+  textInput: {
+    backgroundColor: "#1A2238",
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingTop: pixelSizeVertical(16),
+    paddingBottom: pixelSizeVertical(16),
+    fontSize: fontPixel(16),
+    fontWeight: "400",
+    color: "#DFE5F8",
+    width: "100%",
+    borderRadius: 5,
+    marginTop: pixelSizeVertical(10),
+  },
+  loginButtonLoadingText: {
+    fontSize: fontPixel(22),
+    fontWeight: "400",
+    color: "#DFE5F8",
+    textAlign: "center",
+  },
+  loginButtonDisabled: {
+    backgroundColor: "#1A2238",
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingTop: pixelSizeVertical(18),
+    paddingBottom: pixelSizeVertical(18),
+    marginTop: pixelSizeVertical(16),
+    marginBottom: pixelSizeVertical(24),
+    width: "100%",
+    borderRadius: 5,
+  },
+  tertiaryButton: {
+    color: "#A7AFC7",
+    fontSize: fontPixel(22),
+    textTransform: "lowercase",
+    fontWeight: "400",
+    textAlign: "center",
+  },
+  disclaimer: {
+    marginTop: pixelSizeVertical(-18),
+    fontSize: fontPixel(20),
+    fontWeight: "400",
+    color: "#C6CDE2",
+  },
+  imagePicker: {
+    backgroundColor: "#232F52",
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingTop: pixelSizeVertical(16),
+    paddingBottom: pixelSizeVertical(16),
+    borderRadius: 5,
+  },
+  loginButton: {
+    backgroundColor: "#07BEB8",
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingTop: pixelSizeVertical(18),
+    paddingBottom: pixelSizeVertical(18),
+    marginTop: pixelSizeVertical(16),
+    marginBottom: pixelSizeVertical(24),
+    width: "100%",
+    borderRadius: 5,
+  },
+  loginButtonDisabled: {
+    backgroundColor: "#1A2238",
+    paddingRight: pixelSizeHorizontal(16),
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingTop: pixelSizeVertical(18),
+    paddingBottom: pixelSizeVertical(18),
+    marginTop: pixelSizeVertical(16),
+    marginBottom: pixelSizeVertical(24),
+    width: "100%",
+    borderRadius: 5,
+  },
+  loginButtonText: {
+    fontSize: fontPixel(22),
+    fontWeight: "500",
+    color: "#0C111F",
+    textAlign: "center",
+  },
+  loginButtonLoadingText: {
+    fontSize: fontPixel(22),
+    fontWeight: "400",
+    color: "#DFE5F8",
+    textAlign: "center",
+  },
+  secondaryButton: {
+    fontSize: fontPixel(22),
+    fontWeight: "500",
+    color: "#A7AFC7",
+    marginTop: pixelSizeVertical(2),
+    textAlign: "center",
+  },
+  error: {
+    marginTop: pixelSizeVertical(8),
+    marginBottom: pixelSizeVertical(8),
+    fontSize: fontPixel(12),
+    fontWeight: "400",
+    color: "#a3222d",
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingRight: pixelSizeHorizontal(16),
+  },
+  altButton: {
+    fontSize: fontPixel(22),
+    fontWeight: "500",
+    color: "#07BEB8",
+    marginTop: pixelSizeVertical(8),
+  },
+  altButtonInactive: {
+    fontSize: fontPixel(22),
+    fontWeight: "500",
+    color: "#07BEB8",
+    marginTop: pixelSizeVertical(8),
+    opacity: 0.5,
+  },
+  notificationContainerUnread: {
+    backgroundColor: "#232F52",
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingRight: pixelSizeHorizontal(16),
+    paddingTop: pixelSizeVertical(12),
+    paddingBottom: pixelSizeVertical(12),
+  },
+  notificationContainerRead: {
+    backgroundColor: "#0E1834",
+    paddingLeft: pixelSizeHorizontal(16),
+    paddingRight: pixelSizeHorizontal(16),
+    paddingTop: pixelSizeVertical(12),
+    paddingBottom: pixelSizeVertical(12),
+  },
+  emptyView: {
+    flex: 1,
+    height: pixelSizeVertical(32),
+    backgroundColor: "#0C111F",
+  },
+});
