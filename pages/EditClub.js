a1aaa8058b093d15c39d3a2e293fa61aa15c689d
rajashilan <rajashilan07@gmail.com>
2024-01-01 00:32:32 +0800

    changes(EditClub):
    - integrates primary button component
    - styles cleanup

--- pages/EditClub.js
+++ pages/EditClub.js
@@ -35,6 +35,7 @@ import {
   pixelSizeHorizontal,
 } from "../utils/responsive-font";
 import { toastConfig } from "../utils/toast-config";
+import PrimaryButton from "../components/PrimaryButton";
 
 const { width } = Dimensions.get("window");
 
@@ -130,7 +131,6 @@ export default function EditClub({ navigation }) {
         dispatch(createNotification(notification, userIDs));
       }
     }
-
     setErrors(errors);
   };
 
@@ -343,22 +343,11 @@ export default function EditClub({ navigation }) {
 
             {selectedActive && (
               <>
-                <Pressable
-                  style={
-                    loading ? styles.loginButtonDisabled : styles.loginButton
-                  }
+                <PrimaryButton
+                  loading={loading}
                   onPress={handleEditActiveStatus}
-                >
-                  <Text
-                    style={
-                      loading
-                        ? styles.loginButtonLoadingText
-                        : styles.loginButtonText
-                    }
-                  >
-                    {loading ? "saving changes..." : "save"}
-                  </Text>
-                </Pressable>
+                  text="save"
+                />
                 <Pressable
                   onPress={() => {
                     navigation.goBack();
@@ -405,25 +394,6 @@ const styles = StyleSheet.create({
     paddingRight: pixelSizeHorizontal(16),
     paddingLeft: pixelSizeHorizontal(16),
   },
-  imageHeaderContainer: {
-    height: pixelSizeVertical(120),
-    width: "100%",
-  },
-  overlayContainer: {
-    justifyContent: "center",
-    height: pixelSizeVertical(120),
-    width: "100%",
-    backgroundColor: "rgba(12, 17, 31, 0.7)",
-    paddingRight: pixelSizeHorizontal(16),
-    paddingLeft: pixelSizeHorizontal(16),
-    paddingTop: pixelSizeVertical(16),
-    paddingBottom: pixelSizeVertical(16),
-  },
-  header: {
-    fontSize: fontPixel(34),
-    fontWeight: "500",
-    color: "#DFE5F8",
-  },
   emptyView: {
     flex: 1,
     height: pixelSizeVertical(32),
@@ -434,15 +404,6 @@ const styles = StyleSheet.create({
     width: width * 0.85, // SideMenu width
     alignSelf: "flex-end",
   },
-  headerContainer: {
-    marginTop: pixelSizeVertical(20),
-    marginBottom: pixelSizeVertical(16),
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingRight: pixelSizeHorizontal(16),
-    paddingLeft: pixelSizeHorizontal(16),
-    alignItems: "center",
-  },
   hamburgerIcon: {
     height: pixelSizeVertical(20),
     width: pixelSizeHorizontal(30),
@@ -452,21 +413,6 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     color: "#C4FFF9",
     marginTop: pixelSizeVertical(2),
-  },
-  headerMini: {
-    fontSize: fontPixel(22),
-    fontWeight: "500",
-    color: "#DFE5F8",
-    maxWidth: width - 180,
-    marginLeft: pixelSizeHorizontal(-10),
-  },
-  headerMiniInvisible: {
-    fontSize: fontPixel(22),
-    fontWeight: "500",
-    color: "#DFE5F8",
-    marginRight: pixelSizeHorizontal(16),
-    maxWidth: "80%",
-    opacity: 0,
   },
   headerContainerShowMiniHeader: {
     marginTop: pixelSizeVertical(20),
@@ -484,68 +430,11 @@ const styles = StyleSheet.create({
     marginBottom: pixelSizeVertical(10),
     borderRadius: 5,
   },
-  role: {
-    fontSize: fontPixel(14),
-    fontWeight: "400",
-    color: "#DFE5F8",
-    marginBottom: pixelSizeVertical(4),
-  },
-  name: {
-    fontSize: fontPixel(20),
-    fontWeight: "400",
-    color: "#DFE5F8",
-    marginBottom: pixelSizeVertical(10),
-  },
-  quote: {
-    fontSize: fontPixel(14),
-    fontWeight: "400",
-    color: "#C6CDE2",
-    lineHeight: 22,
-  },
-  textInput: {
-    backgroundColor: "#1A2238",
-    paddingRight: pixelSizeHorizontal(16),
-    paddingLeft: pixelSizeHorizontal(16),
-    paddingTop: pixelSizeVertical(16),
-    paddingBottom: pixelSizeVertical(16),
-    fontSize: fontPixel(16),
-    fontWeight: "400",
-    color: "#DFE5F8",
-    width: "100%",
-    borderRadius: 5,
-    marginTop: pixelSizeVertical(10),
-  },
-  tertiaryButton: {
-    color: "#A7AFC7",
-    fontSize: fontPixel(22),
-    textTransform: "lowercase",
-    fontWeight: "400",
-    textAlign: "center",
-  },
   disclaimer: {
     marginTop: pixelSizeVertical(-18),
     fontSize: fontPixel(20),
     fontWeight: "400",
     color: "#C6CDE2",
-  },
-  imagePicker: {
-    backgroundColor: "#232F52",
-    paddingRight: pixelSizeHorizontal(16),
-    paddingLeft: pixelSizeHorizontal(16),
-    paddingTop: pixelSizeVertical(16),
-    paddingBottom: pixelSizeVertical(16),
-    borderRadius: 5,
-  },
-  loginButton: {
-    backgroundColor: "#07BEB8",
-    paddingRight: pixelSizeHorizontal(16),
-    paddingLeft: pixelSizeHorizontal(16),
-    paddingTop: pixelSizeVertical(18),
-    paddingBottom: pixelSizeVertical(18),
-    marginTop: pixelSizeVertical(16),
-    marginBottom: pixelSizeVertical(24),
-    width: "100%",
-    borderRadius: 5,
   },
   secondaryButton: {
     fontSize: fontPixel(22),
