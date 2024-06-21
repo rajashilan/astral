e364959a673a6e15868bcec0ce73d665433ab28e
rajashilan <rajashilan07@gmail.com>
2024-06-21 22:37:35 +0800

    fix: issue with eas update

--- App.js
+++ App.js
@@ -79,30 +79,30 @@ const firebaseAppCheckToken = async () => {
 
 firebaseAppCheckToken();
 
-async function onFetchUpdateAsync() {
-  try {
-    const update = await Updates.checkForUpdateAsync();
-
-    if (update.isAvailable) {
-      await Updates.fetchUpdateAsync();
-      await Updates.reloadAsync();
-    }
-  } catch (error) {
-    // You can also add an alert() to see the error message in case of an error when fetching updates.
-    alert(`Error fetching latest Expo update: ${error}`);
-  }
-}
-
-useEffect(() => {
-  onFetchUpdateAsync();
-}, []);
-
 const animationConfig = {
   animation: "timing",
   config: { duration: 0 },
 };
 
 export default function App() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    onFetchUpdateAsync();
+  }, []);
+
   return (
     <Provider store={store}>
       <NavigationContainer
