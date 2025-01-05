0ee0abfd98b2313ae9337c5e764d278e16750833
rajashilan <91580670+rajashilan@users.noreply.github.com>
2025-01-06 01:34:18 +0800

    update(Signup): intake years and schools

--- pages/Signup.js
+++ pages/Signup.js
@@ -44,7 +44,8 @@ export default function Signup({ navigation }) {
 
   const [intakeMonths, setIntakeMonths] = useState([]);
   const [intakeYears] = useState([
-    2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024,
+    2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
+    2019, 2020, 2021, 2022, 2023, 2024, 2025,
   ]);
   const [selectedIntakeMonth, setSelectedIntakeMonth] = useState("");
   const [selectedIntakeYear, setSelectedIntakeYear] = useState(0);
@@ -271,8 +272,8 @@ export default function Signup({ navigation }) {
               onSelect={(selectedItem, index) => {
                 setSelectedDepartment(selectedItem);
               }}
-              defaultText="Select your department"
-              loadingText="Loading departments..."
+              defaultText="Select your school"
+              loadingText="Loading schools..."
               loading={loadingCampusDetails}
               ref={departmentDropDownRef}
             />
